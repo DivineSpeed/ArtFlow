59bef300f496a34e4c0b53d9cfe9470c6a3b1f9d
yassine-mathlouthi <yassinemt08@gmail.com>
2024-12-08 02:51:05 +0100

    add admin guard routes

--- frontend/src/app/guards/role.guard.ts
+++ frontend/src/app/guards/role.guard.ts
@@ -2,13 +2,13 @@ import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const roleGuard: CanActivateFn = (route, state) => {
   const router = inject(Router); // Inject Router service
 
   const token = sessionStorage.getItem('token');
   const role = sessionStorage.getItem('role');
 
-  if (token && role == "ARTISAN") {
+  if (token && role == "ADMIN") {
     return true; // Allow navigation if token is found
   } else if(!token){
     router.navigate(['/login']); // Redirect to login if no token
